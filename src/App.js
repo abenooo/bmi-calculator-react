17c792ef0c14a044a58a87dced98a3ae354380f6
aR <abenezerkifle000@gmail.com>
2022-10-08 09:09:33 +0300

    Fineshd the layout

--- src/App.js
+++ src/App.js
@@ -2,10 +2,46 @@
 import React, {useState} from 'react';
 import './index.css'
 function App() {
+    // state
+    const [weight, setWeight] = useState(0)
+    const [height, setHeight] = useState(0)
+    const [bmi, setBmi] = useState('')
+    const [message, setMessage] = useState('')
+
+    const imgSrc = ''
+    // reload the browser when the user want to clean the form
+    let reload = () => {
+      window.location.reload()
+    }
   return (
-    <div className="App">
-    
+    <div className="app">
+    <div className='container'>
+      <h2 className='center'>BMI Calculator</h2>
+      <form >
+        <div>
+          <label>Weight (lbs)</label>
+          <input value={weight} onChange={(e) => setWeight(e.target.value)} />
+        </div>
+        <div>
+          <label>Height (in)</label>
+          <input value={height} onChange={(event) => setHeight(event.target.value)} />
+        </div>
+        <div>
+          <button className='btn' type='submit'>Submit</button>
+          <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+        </div>
+      </form>
+
+      <div className='center'>
+        <h3>Your BMI is: {bmi}</h3>
+        <p>{message}</p>
+      </div>
+
+      <div className='img-container'>
+        <img src={imgSrc} alt=''></img>
+      </div>
     </div>
+  </div>
   );
 }
 
